30654c6e18d0257d41bccf898f0c6b74e8929937
tarang <rastogitarang5@gmail.com>
2025-10-28 15:08:04 +0530

    updates model payer and application.properties

---
+++ url-shortner-sb/src/main/java/com/ulr/shortner/models/User.java
@@ -0,0 +1,19 @@
+package com.ulr.shortner.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role = "ROLE_USER";
+
+}
